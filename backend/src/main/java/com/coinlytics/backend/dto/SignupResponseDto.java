8b67ae2c0350048bc457b78a5ac0bfd51ce5f8d9
CyberPokemon <imoncoding@gmail.com>
2026-05-24 00:02:26 +0530

    Signup response dto added

--- backend/src/main/java/com/coinlytics/backend/dto/SignupResponseDto.java
+++ backend/src/main/java/com/coinlytics/backend/dto/SignupResponseDto.java
@@ -3,7 +3,8 @@ package com.coinlytics.backend.dto;
 import lombok.Data;
 
 @Data
-public class LoginResponseDto {
-    String jwt;
+public class SignupResponseDto {
+    
     private String username;
+    private String accessToken;
 }
