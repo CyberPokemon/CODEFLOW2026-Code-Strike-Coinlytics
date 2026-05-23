3f50d5ee3e2536ff278b977e29f51e6ad682b283
CyberPokemon <imoncoding@gmail.com>
2026-05-24 02:23:39 +0530

    Aes Utility added

---
+++ backend/src/main/java/com/coinlytics/backend/service/AESUtil.java
@@ -0,0 +1,40 @@
+package com.coinlytics.backend.service;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESUtil {
+
+    private static final String ALGORITHM = "AES";
+
+    // 16 bytes = 128-bit AES key
+    private static final String SECRET =
+            "1234567890123456";
+
+    private static final SecretKey secretKey =
+            new SecretKeySpec(
+                    SECRET.getBytes(),
+                    ALGORITHM
+            );
+
+    public static byte[] encrypt(byte[] data)
+            throws Exception {
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decrypt(byte[] encryptedData)
+            throws Exception {
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        return cipher.doFinal(encryptedData);
+    }
+}
